0c053496d35835cb9766da3c1ee3716099b98c87
trizyco <122378391+trizyco@users.noreply.github.com>
2023-02-27 13:21:18 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server -> bot's response
 
-  const response = await fetch('http://sv.trizy.co', {
+  const response = await fetch('http://sv.trizy.co:4040', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
